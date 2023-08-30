ce91912a503e76b2b63138e5c2fb41a578e05ab2
Rebekka Rustad <79134327+rebekkarustad@users.noreply.github.com>
2023-08-31 00:19:34 +0200

    Last projects

--- js/components/createMenu.js
+++ js/components/createMenu.js
@@ -1,8 +1,8 @@
 const header = document.querySelector("header");
 
-export function createMenu() {  
-    function navigationMenu() {
-        header.innerHTML = `<nav class="navbar">
+export function createMenu() {
+  function navigationMenu() {
+    header.innerHTML = `<nav class="navbar">
                                 <a href="index.html" class="nav-branding">R</a>
 
                                 <ul class="nav-menu">
@@ -11,7 +11,6 @@ export function createMenu() {
                                     <li class="nav-item"><a href="skills.html" class="nav-link">Skills and tools</a></li>
                                     <li class="nav-item"><a href="portfolio.html" class="nav-link">Portfolio</a></li>
                                     <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-                                    <i class="fa-solid fa-moon"></i>
                                 </ul>
 
                                 <div class="hamburger">
@@ -19,30 +18,32 @@ export function createMenu() {
                                     <span class="bar"></span>
                                     <span class="bar"></span>
                                 </div>
-                            </nav>`
-    }
-    navigationMenu()
+                            </nav>`;
+  }
+  navigationMenu();
 
-    const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector(".nav-menu");
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
 
-    hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("active");
-        navMenu.classList.toggle("active");
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+
+  document.querySelectorAll(".nav-link").forEach((n) =>
+    n.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
     })
+  );
 
-    document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }))
+  const currentLocation = location.href;
+  const menuItem = document.querySelectorAll(".nav-menu a");
+  const menuLenght = menuItem.length;
 
-    const currentLocation = location.href;
-    const menuItem = document.querySelectorAll(".nav-menu a");
-    const menuLenght = menuItem.length;
-
-    for (let i = 0; i<menuLenght;  i++) {
-        if (menuItem[i].href === currentLocation) {
-            menuItem[i].className = "current"
-        }
+  for (let i = 0; i < menuLenght; i++) {
+    if (menuItem[i].href === currentLocation) {
+      menuItem[i].className = "current";
     }
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 import { createMenu } from "./components/createMenu.js";
-import { createFooter } from "./components/createFooter.js"
+
+import { createFooter } from "./components/createFooter.js";
 
 createMenu();
 createFooter();
